4772f370866b1de3cebb67174e3055dc60fd8d8c
Tong Lin <tong.lin@gpshopper.com>
2018-02-22 16:13:39 -0500

    still setting up project

---
+++ biubiubiu/Loading/LoadingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoadingViewController.swift
+//  biubiubiu
+//
+//  Created by Tong Lin on 2/22/18.
+//  Copyright © 2018 tonglin.t91@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: SuperViewController {
+    //Loading animation on app launch
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
